b539e4f0029c025e39f2be287394487ab3bbd1a0
Raguel L. Rivera <riv5181@gmail.com>
2016-10-18 18:13:27 +0800

    Updated codes and stuff - Rag

--- Preprocessor/packetReceiver.py
+++ Preprocessor/packetReceiver.py
@@ -1,65 +1,168 @@
-# Packet sniffer in python for Linux
-# Sniffs only incoming TCP packet
+'''
+Packet sniffer in python using the pcapy python library
 
-import socket, sys
+Project website
+http://oss.coresecurity.com/projects/pcapy.html
+'''
+
+import socket
 from struct import *
+import datetime
+import pcapy
+import sys
 
-# create an INET, STREAMing socket
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
-except socket.error as msg:
-    print ('Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
-    sys.exit()
+def main(argv):
+    # list all devices
+    devices = pcapy.findalldevs()
+    print (devices)
 
-# receive a packet
-while True:
-    packet = s.recvfrom(65565)
+    # ask user to enter device name to sniff
+    print ("Available devices are :")
+    for d in devices:
+        print (d)
 
-    # packet string from tuple
-    packet = packet[0]
+    dev = raw_input("Enter device name to sniff : ")
 
-    # take first 20 characters for the ip header
-    ip_header = packet[0:20]
+    print ("Sniffing device " + dev)
 
-    # now unpack them :)
-    iph = unpack('!BBHHHBBH4s4s', ip_header)
+    '''
+    open device
+    # Arguments here are:
+    #   device
+    #   snaplen (maximum number of bytes to capture _per_packet_)
+    #   promiscious mode (1 for true)
+    #   timeout (in milliseconds)
+    '''
+    cap = pcapy.open_live(dev, 65536, 1, 0)
 
-    version_ihl = iph[0]
-    version = version_ihl >> 4
-    ihl = version_ihl & 0xF
+    # start sniffing packets
+    while (1):
+        (header, packet) = cap.next()
+        # print ('%s: captured %d bytes, truncated to %d bytes' %(datetime.datetime.now(), header.getlen(), header.getcaplen()))
+        parse_packet(packet)
 
-    iph_length = ihl * 4
 
-    ttl = iph[5]
-    protocol = iph[6]
-    s_addr = socket.inet_ntoa(iph[8]);
-    d_addr = socket.inet_ntoa(iph[9]);
+# Convert a string of 6 characters of ethernet address into a dash separated hex string
+def eth_addr(a):
+    b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (ord(a[0]), ord(a[1]), ord(a[2]), ord(a[3]), ord(a[4]), ord(a[5]))
+    return b
 
-    print ('Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(
-        protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr))
 
-    tcp_header = packet[iph_length:iph_length + 20]
+# function to parse a packet
+def parse_packet(packet):
+    # parse ethernet header
+    eth_length = 14
 
-    # now unpack them :)
-    tcph = unpack('!HHLLBBHHH', tcp_header)
+    eth_header = packet[:eth_length]
+    eth = unpack('!6s6sH', eth_header)
+    eth_protocol = socket.ntohs(eth[2])
+    print ('Destination MAC : ' + eth_addr(packet[0:6]) + ' Source MAC : ' + eth_addr(packet[6:12]) + ' Protocol : ' + str(
+        eth_protocol))
 
-    source_port = tcph[0]
-    dest_port = tcph[1]
-    sequence = tcph[2]
-    acknowledgement = tcph[3]
-    doff_reserved = tcph[4]
-    tcph_length = doff_reserved >> 4
+    # Parse IP packets, IP Protocol number = 8
+    if eth_protocol == 8:
+        # Parse IP header
+        # take first 20 characters for the ip header
+        ip_header = packet[eth_length:20 + eth_length]
 
-    print ('Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(
-        sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length))
+        # now unpack them :)
+        iph = unpack('!BBHHHBBH4s4s', ip_header)
 
-    h_size = iph_length + tcph_length * 4
-    data_size = len(packet) - h_size
+        version_ihl = iph[0]
+        version = version_ihl >> 4
+        ihl = version_ihl & 0xF
 
-    # get data from the packet
-    data = packet[h_size:]
+        iph_length = ihl * 4
 
-    print ('Data : ' + data)
-    print
-    print
+        ttl = iph[5]
+        protocol = iph[6]
+        s_addr = socket.inet_ntoa(iph[8]);
+        d_addr = socket.inet_ntoa(iph[9]);
+
+        print ('Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(
+            protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr))
+
+        # TCP protocol
+        if protocol == 6:
+            t = iph_length + eth_length
+            tcp_header = packet[t:t + 20]
+
+            # now unpack them :)
+            tcph = unpack('!HHLLBBHHH', tcp_header)
+
+            source_port = tcph[0]
+            dest_port = tcph[1]
+            sequence = tcph[2]
+            acknowledgement = tcph[3]
+            doff_reserved = tcph[4]
+            tcph_length = doff_reserved >> 4
+
+            print ('Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(
+                sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length) + ' PCKT: TCP')
+
+            h_size = eth_length + iph_length + tcph_length * 4
+            data_size = len(packet) + h_size #includes packet header size
+
+            # get data from the packet
+            data = packet[h_size:]
+
+            print ('Data Size: ' + str(data_size) + ' bytes')
+
+        # ICMP Packets
+        elif protocol == 1:
+            u = iph_length + eth_length
+            icmph_length = 4
+            icmp_header = packet[u:u + 4]
+
+            # now unpack them :)
+            icmph = unpack('!BBH', icmp_header)
+
+            icmp_type = icmph[0]
+            code = icmph[1]
+            checksum = icmph[2]
+
+            print ('Type : ' + str(icmp_type) + ' Code : ' + str(code) + ' Checksum : ' + str(checksum) + ' PCKT: ICMP')
+
+            h_size = eth_length + iph_length + icmph_length
+            data_size = len(packet) + h_size
+
+            # get data from the packet
+            data = packet[h_size:]
+
+            print ('Data Size: ' + str(data_size) + ' bytes')
+
+        # UDP packets
+        elif protocol == 17:
+            u = iph_length + eth_length
+            udph_length = 8
+            udp_header = packet[u:u + 8]
+
+            # now unpack them :)
+            udph = unpack('!HHHH', udp_header)
+
+            source_port = udph[0]
+            dest_port = udph[1]
+            length = udph[2]
+            checksum = udph[3]
+
+            print ('Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Length : ' + str(
+                length) + ' Checksum : ' + str(checksum) + ' PCKT: UDP')
+
+            h_size = eth_length + iph_length + udph_length
+            data_size = len(packet) + h_size
+
+            # get data from the packet
+            data = packet[h_size:]
+
+            print ('Data Size: ' + str(data_size) + ' bytes')
+
+        # some other IP packet like IGMP
+        else:
+            print ('Protocol other than TCP/UDP/ICMP')
+
+        print
+
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ classes.py
@@ -0,0 +1,18 @@
+class packet:
+    sourceIP = ''
+    destIP = ''
+    sourceMAC = ''
+    destMAC = ''
+    protocol = ''
+    service = ''
+    size = ''
+
+    def __init__(self,sourceIP,destIP,sourceMAC,destMAC,protocol,service,size):
+        self.sourceIP = sourceIP
+        self.destIP = destIP
+        self.sourceMAC = sourceMAC
+        self.destMAC = destMAC
+        self.protocol = protocol
+        self.service = service
+        self.size = size
+
